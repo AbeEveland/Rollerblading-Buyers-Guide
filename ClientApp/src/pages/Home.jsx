c588c80fd3595f5362a50c4d0f34687b4a41b802
Abe Eveland <abeeveland@gmail.com>
2020-07-17 11:48:34 -0400

    Static HTML and CSS complete

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,62 +1,82 @@
 import React from 'react'
+import { Route, Switch } from 'react-router'
+import SkillLevel from '../pages/_template/SkillLevel'
+import SkatingStyle from '../pages/_template/SkatingStyle'
+import SkatingEnviroment from '../pages/_template/SkatingEnviroment'
+import BootMaterial from '../pages/_template/BootMaterial'
+import Wheels from '../pages/_template/Wheels'
+import Frame from '../pages/_template/Frame'
+import BuyersGuide from '../pages/_template/Buyers Guide'
 
 export function Home() {
   return (
-    <div>
-      <h1>Hello Student!</h1>
-      <p>Welcome to your new SDG SPA, built with:</p>
-      <ul>
+    <>
+      {/* <div className="homePageContainer"> */}
+      {/* <img
+          className="logo"
+          src="https://store-images.s-microsoft.com/image/apps.14716.9007199266365238.ab475298-d17b-437b-b143-0537efcad818.aabd06dc-87c7-47a9-addf-1e26155db4b4?mode=scale&q=90&h=300&w=300"
+          alt=""
+          width="30px"
+        /> */}
+      <div className="homePageHeader">
+        <h1 className="headinghH1">ROLLERBLADE WAREHOUSE</h1>
+        <button className="homePageAccountButtons">
+          <img
+            className="homePageAccountButton"
+            src="https://thumbs.dreamstime.com/b/user-account-line-icon-outline-person-logo-illustration-linear-pictogram-isolated-white-90234649.jpg"
+            alt="Account"
+            width="20px"
+          />
+        </button>
+      </div>
+      <div className="thingsToConsider">
+        <h1>Rollerblade Buying Guide</h1>
+        <p>
+          With a wide range of inline skates built for varying types of skating,
+          it can be difficult to find the skate that is best suited for you.
+          This guide will take you through the many options presented on our
+          site to help you use our filters and product descriptions to narrow
+          down the field. The main factors we will guide you through on this
+          page are:
+        </p>
         <li>
-          <a href="https://get.asp.net/">ASP.NET Core</a> and{' '}
-          <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-            C#
-          </a>{' '}
-          for cross-platform server-side code
+          <a href="/SkillLevel"> Skill Level</a>
         </li>
         <li>
-          <a href="https://facebook.github.io/react/">React</a> for client-side
-          code
+          <a href="/SkatingStyle">Skating style</a>
         </li>
         <li>
-          <a href="https://www.typescriptlang.org/">Typescript</a> for better
-          client-side code
+          {' '}
+          <a href="/SkatingEnviroment">Skating Enviroment</a>
         </li>
         <li>
-          <a href="https://sass-lang.com/">Sass</a> for better css
+          {' '}
+          <a href="/BootMaterial">Boot Types </a>
         </li>
         <li>
-          <a href="http://getbootstrap.com/">Bootstrap</a> for layout and
-          styling
-        </li>
-      </ul>
-      <p>To help you get started, we have also set up:</p>
-      <ul>
-        <li>
-          <strong>Client-side navigation</strong>.
+          {' '}
+          <a href="/Wheels">Wheels </a>
         </li>
         <li>
-          <strong>Development server integration</strong>. In development mode,
-          the development server from <code>create-react-app</code> runs in the
-          background automatically, so your client-side resources are
-          dynamically built on demand and the page refreshes when you modify any
-          file.
+          {' '}
+          <a href="/Frame"> Frame</a>
         </li>
-        <li>
-          <strong>Efficient production builds</strong>. In production mode,
-          development-time features are disabled, and your{' '}
-          <code>dotnet publish</code> configuration produces minified,
-          efficiently bundled JavaScript files.
-        </li>
-        <li>
-          <strong>Docker</strong>.
-        </li>
-      </ul>
-      <p>
-        The <code>ClientApp</code> subdirectory is a standard React application
-        based on the <code>create-react-app</code> template. If you open a
-        command prompt in that directory, you can run <code>npm</code> commands
-        such as <code>npm test</code> or <code>npm install</code>.
-      </p>
-    </div>
+
+        <a>
+          {' '}
+          <a href="/BuyersGuide"> BuyersGuide</a>
+        </a>
+      </div>
+      {/* </div> */}
+      <Switch>
+        <Route exact path="/SkillLevel" component={SkillLevel} />
+        <Route exact path="/SkatingStyle" component={SkatingStyle} />
+        <Route exact path="/SkatingEnviroment" component={SkatingEnviroment} />
+        <Route exact path="/BootMaterial" component={BootMaterial} />
+        <Route exact path="/Wheels" component={Wheels} />
+        <Route exact path="/Frame" component={Frame} />
+        <Route exact path="/BuyersGuide" component={BuyersGuide} />
+      </Switch>
+    </>
   )
 }
